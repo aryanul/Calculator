e1e36b279e5b7d79190a9b42acc73e8fd787f37a
Khandakar Aryanul Haque <123832026+aryanul@users.noreply.github.com>
2023-08-26 10:48:06 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-def intro():
+def calculate():
     #taking input from user for the numbers to calculate
     a= int(input("Enter the first number: "))
     b= int(input("Enter the second number: "))
@@ -6,23 +6,23 @@ def intro():
     2.Subtraction
     3.Multiplication
     4.Division''')
-intro()
-#taking the operation as input from the user
-#using loop s that in case user has multiple operation they donot need to start over again
-while True:
-    c=(input("Choose the operation(1/2/3/4/stop):"))
-    if c=='1':
-        print( a,"+",b,"=",a+b)
-    elif c=='2':
-        print( a,"-",b,"=",a-b)
-    elif c=='3':
-        print( a,"*",b,"=",a*b)
-    elif c=='4':
-        print(a,"/",b,"=",a/b)
-    elif c=='stop':
-        break
-    elif c=='restart':
-        intro()
-    else:
-        print("Error: invalid input.")
-    
+    #taking the operation as input from the user
+    #using loop so that in case user has multiple operation they donot need to start over again
+    while True:
+        c=(input("Choose the operation(1/2/3/4/stop/restart):"))
+        if c=='1':
+            print( a,"+",b,"=",a+b)
+        elif c=='2':
+            print( a,"-",b,"=",a-b)
+        elif c=='3':
+            print( a,"*",b,"=",a*b)
+        elif c=='4':
+            print(a,"/",b,"=",a/b)
+        elif c=='stop':
+            break
+        elif c=='restart':
+            calculate()
+        else:
+            print("Error: invalid input.")
+            
+calculate()
