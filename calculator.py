571a8e27579c8cb585de8d88bbf2460f4933120d
Khandakar Aryanul Haque <123832026+aryanul@users.noreply.github.com>
2023-08-26 10:43:34 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,14 @@
-#taking input from user for the numbers to calculate
-a= int(input("Enter the first number: "))
-b= int(input("Enter the second number: "))
-print('''1.Addition
-2.Subtraction
-3.Multiplication
-4.Division''')
+def intro():
+    #taking input from user for the numbers to calculate
+    a= int(input("Enter the first number: "))
+    b= int(input("Enter the second number: "))
+    print('''1.Addition
+    2.Subtraction
+    3.Multiplication
+    4.Division''')
+intro()
 #taking the operation as input from the user
+#using loop s that in case user has multiple operation they donot need to start over again
 while True:
     c=(input("Choose the operation(1/2/3/4/stop):"))
     if c=='1':
@@ -18,6 +21,8 @@ while True:
         print(a,"/",b,"=",a/b)
     elif c=='stop':
         break
+    elif c=='restart':
+        intro()
     else:
         print("Error: invalid input.")
     
